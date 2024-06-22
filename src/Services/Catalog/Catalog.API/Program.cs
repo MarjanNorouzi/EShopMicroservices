ed213a7bd34fdf3051187ace138954d4dbe51001
MarjanNorouzi <Marjan33norouzi@gmail.com>
2024-06-22 18:56:41 +0330

    ...

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Configure the Http request pipeline.
 
 app.Run();
