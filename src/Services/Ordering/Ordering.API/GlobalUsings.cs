fc1c589199dbbd368d345a070ce2b0b1b1a781f0
Marjan Norouzi <marjan33norouzi@gmail.com>
2024-07-22 01:09:23 +0330

    ...

--- src/Services/Ordering/Ordering.API/GlobalUsings.cs
+++ src/Services/Ordering/Ordering.API/GlobalUsings.cs
@@ -1,5 +1,10 @@
-﻿global using Ordering.API;
+﻿global using BuildingBlocks.Pagination;
+global using Carter;
+global using Mapster;
+global using MediatR;
+global using Ordering.API;
 global using Ordering.Application;
+global using Ordering.Application.Dtos;
 global using Ordering.Infrastructure;
 global using Ordering.Infrastructure.Data.Extensions;
 
